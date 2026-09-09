a55ce3176f3cfa76034bbaf806a0c5b957e956d8
genotaletime <genotaletime@gmail.com>
2026-09-09 18:42:22 +0100

    Update forge_construct_weapons.lua

--- warhammer_misc_configs/forge_construct_weapons.lua
+++ warhammer_misc_configs/forge_construct_weapons.lua
@@ -152,5 +152,6 @@ CRAFTABLE_ITEMS["item_561368"] = 250000 -- Kastelan Arc Cannon
 CRAFTABLE_ITEMS["item_948321"] = 25000 -- Arc Lance & Shield
 CRAFTABLE_ITEMS["item_232826"] = 25000 -- Arc Maul & Shield
 CRAFTABLE_ITEMS["item_839748"] = 17500 -- Transonic Blade
+CRAFTABLE_ITEMS["item_664305"] = 22500 -- Dual Transonic Blades
 CRAFTABLE_ITEMS["item_405405"] = 17500 -- Arc Maul
 CRAFTABLE_ITEMS["item_619833"] = 12500 -- Taser Goad
